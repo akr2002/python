77fcf3d127e2d946a553b3a7ea4e6304befba627
Aditya <adityakr082@gmail.com>
2020-03-21 17:42:13 +0530

    added user input

--- calculator.py
+++ calculator.py
@@ -1,2 +1,10 @@
-print("Which numbers do you want to add?")
+def add(num1, num2):
+    return (num1 + num2)
+
+print("Enter a number: ")
+int n1 = input();
+print("Now, enter another another number: ")
+int n2 = input();
+print("The added numbers are: ")
+add(n1, n2)
 
