c7b8cfc1dac6127739c6e38c679b862045bc0b71
GNUxeava <bluenerd@protonmail.com>
2021-07-21 10:38:48 +0530

    LinkedList - traversal

--- LinkedList.py
+++ LinkedList.py
@@ -1,9 +1,14 @@
 class Node:
     def __init__(self, dataVal = None):
         self.dataVal = dataVal
-        self.dataVal = None 
+        self.nestVal = None 
 
 class SLinkedList:
     def __init__(self):
         self.headval = None 
 
+    def listPrint(self):
+        printVal = self.headval 
+        while printVal is not None:
+            print(printVal.dataVal)
+            printVal = printVal.nextVal
