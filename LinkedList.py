806c168ed3b13a88bc739b73b37bab734488a8ad
GNUxeava <bluenerd@protonmail.com>
2021-07-21 10:34:41 +0530

    LinkedList - creation

---
+++ LinkedList.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, dataVal = None):
+        self.dataVal = dataVal
+        self.dataVal = None 
+
+class SLinkedList:
+    def __init__(self):
+        self.headval = None 
+
