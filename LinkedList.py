4188ef36876db94174056412ccc1dab623eeb353
GNUxeava <bluenerd@protonmail.com>
2021-07-21 10:44:03 +0530

    LinkedList - insertion

--- LinkedList.py
+++ LinkedList.py
@@ -1,14 +1,22 @@
 class Node:
     def __init__(self, dataVal = None):
         self.dataVal = dataVal
-        self.nestVal = None 
+        self.nextVal = None 
 
 class SLinkedList:
     def __init__(self):
         self.headval = None 
 
+    # Print
     def listPrint(self):
         printVal = self.headval 
         while printVal is not None:
             print(printVal.dataVal)
             printVal = printVal.nextVal
+
+    # Insert
+    def AtBeginning(self, newData):
+        NewNode = Node(newData);
+        # Update the new node's nextVal to existing node
+        NewNode.nextVal = self.headval 
+        self.headval = NewNode
