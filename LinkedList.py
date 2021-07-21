d5a9a204b76f4eaea29bcf3c8e60874804995313
GNUxeava <bluenerd@protonmail.com>
2021-07-21 10:48:40 +0530

    LinkedList - insertion at end

--- LinkedList.py
+++ LinkedList.py
@@ -8,15 +8,26 @@ class SLinkedList:
         self.headval = None 
 
     # Print
-    def listPrint(self):
+    def printList(self):
         printVal = self.headval 
         while printVal is not None:
             print(printVal.dataVal)
             printVal = printVal.nextVal
 
-    # Insert
-    def AtBeginning(self, newData):
+    # Insert at beginning
+    def atBeginning(self, newData):
         NewNode = Node(newData);
         # Update the new node's nextVal to existing node
         NewNode.nextVal = self.headval 
         self.headval = NewNode
+
+    # Insert at end
+    def atEnd(self, newData):
+        NewNode = Node(newData)
+        if self.headval is None:
+            self.headval = NewNode
+            return
+        last = self.headval
+        while (last.nextVal):
+            last = last.nextVal
+        last.nextVal = NewNode
