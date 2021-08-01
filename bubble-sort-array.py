fd114d57327ee568f91bbcb492c42821161c49aa
GNUxeava <bluenerd@protonmail.com>
2021-08-01 16:53:32 +0530

    sort - initial

---
+++ bubble-sort-array.py
@@ -0,0 +1,22 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if swapped == False:
+            break 
+
+    return arr;
+
+# Driver code
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+
+print("Sorted array:", bubbleSort(arr))
